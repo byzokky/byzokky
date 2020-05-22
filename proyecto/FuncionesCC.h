7be78fcfe509228b40961169e9f016be4c842d84
byzokky <57722802+byzokky@users.noreply.github.com>
2020-05-22 11:53:28 -0500

    primeras mejoras

---
+++ proyecto/FuncionesCC.h
@@ -0,0 +1,44 @@
+#ifndef FUNCIONESCC_H_
+#define FUNCIONESCC_H_
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+//Local 
+typedef struct Local{
+	char nombreLocal[35];
+	int idLocal; // Calculado automaticamente por su programa
+	int pisoLocal;
+	int numLocalxPiso;//Columnas
+	int precio;
+	int GTotal;
+	// Completelo con lo que quiera
+} local_t;
+
+
+/*Agregar las funciones que necesite para satisfacer los requerimientos */
+void mostrarLocal(local_t ** centroComercial, int numPisos, int numLocalesxPiso);
+
+int idLocal();
+
+int menu();
+
+int menuEdit();
+
+void alquilarLocal(local_t **centroComercial, int pisos, int locales);
+
+void eliminarLocal(local_t **centroComercial, int pisos, int locales);
+
+void editarLocal(local_t **centroComercial, int pisos, int locales);
+
+void TopVentas(local_t **centroComercial, int pisos, int locales);
+
+void comprarEn(local_t **centroComercial, int pisos, int locales);
+
+void gananciasCC(local_t **centroComercial, int pisos, int locales);
+
+int planosCC(local_t **centroComercial, int pisos, int locales);
+
+
+#endif /* FUNCIONESCC_H_ */
---
+++ proyecto/centroComercial.c
@@ -0,0 +1,94 @@
+#include "FuncionesCC.h"
+#include <stdio.h>
+
+int main(){
+	int opc;
+	int i;
+	int j;
+	int pisos;
+	int locales;
+    printf("cantidad de pisos: ");
+    scanf("%d", &pisos);
+    printf("cantidad de locales: ");
+    scanf("%d", &locales);
+	local_t centroComercial =  malloc(sizeof(local_t*) * pisos);
+	
+    for(i = 0; i < pisos; i++){
+        centroComercial[i] = malloc(sizeof(local_t) * locales);
+        for (j = 0; j < pisos; j++)
+            centroComercial[i][j].idLocal = 0;
+    }
+    
+    FILE *r = fopen( "reporte.txt", "rb" ); 
+	fclose( r );
+
+	do{
+		opc = menu();
+		switch(opc){
+		case 1: mostrarLocal(centroComercial, pisos, locales);
+				break;
+		
+		
+		case 2: alquilarLocal(centroComercial, pisos, locales);
+				break;
+		
+		
+		case 3: eliminarLocal(centroComercial, pisos, locales);
+				break;
+		
+		
+		case 4: r = fopen( "reporte.txt", "w" );
+				fprintf( r, "REPORTE LOCALES DEL CENTRO VERTICAL DE AUTOGESTION\n" );
+				fprintf( r, "==================================================\n" );
+				for (i = 0; i < pisos; i++){
+			        for (j = 0; j < locales; j++){
+			        	if(centroComercial[i][j] !=0){
+			            	fprintf("Nombre del local es %s\n", centroComercial[i][j].nombreLocal);
+			            	fprintf("Local numero %d del piso %d\n", j, i);
+			            	fprintf("Id del local es %d\n", centroComercial[i][j].idLocal);
+			            	fprintf("Precio manejado por el local es %d\n", centroComercial[i][j].precip);
+			            	fprintf("Ganancias totales del local es %d\n", centroComercial[i][j].GTotal);
+						}
+						else
+							fprintf("local [%d][%d] libre para alquilar\n", i, j);
+					}
+				}
+				fclose( r );
+				printf( "Se genero reporte.txt.\n" );
+				system( "notepad reporte.txt" );
+				break;
+				
+				
+		case 5: editarLocal(centroComercial, pisos, locales);
+				break;
+		
+		
+		case 6: TopVentas(centroComercial);
+				break;
+				
+				
+		case 7: comprarEn(centroComercial, pisos, locales);
+				break;
+				
+				
+		case 8: gananciasCC(centroComercial, pisos, locales);
+				break;
+				
+				
+		case 9: int disponibles = planosCC(centroComercial, pisos, locales);
+				printf("\n%d locales libre", disponible);
+				break;
+				
+		
+		case 0: printf ("gracias por venir");
+				break;
+				
+				
+		default:
+			printf("opcion invalida\n");
+			break;
+			
+		}while (opc!=0);
+	}
+	return 0;
+}
